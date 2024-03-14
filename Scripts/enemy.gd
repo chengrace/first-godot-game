753ebf5ef316d7ddc7e2358076a5e0fbfe13ea8d
chengrace <g.chen@rutgers.edu>
2024-03-14 16:40:03 -0400

    enemy created and randomized walking
    however it does not work right now:
    - issues include: collisions causing errors and following the player

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,23 +1,39 @@
 ## enemy.gd
 
-extends AnimatedSprite2D
- 
+extends CharacterBody2D
+
+var is_following = false
+var direction = Vector2.ONE
+var movements = [Vector2.LEFT, Vector2.RIGHT, Vector2.UP, Vector2.DOWN]
+var rng = RandomNumberGenerator.new()
+
 func _ready():
+	rng.randomize()
 	position = position.snapped(Vector2.ONE * Constants.TILE_SIZE)
 	position -= Vector2.ONE * (Constants.TILE_SIZE / 2)
-	play("walk_side") #todo: change to idle_down after animation sheet is added
+	$AnimatedSprite2D.play("walk_side") #todo: change to idle_down after animation sheet is added
 	#play("idle_down")
  
 func _process(_delta):
-	# follow player if close enough
-	
-	# randomized walking
-	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	$GridMovement.move(input_direction)
-	moving_animation(input_direction)
+	# if enemy touches player, trigger battle scene.
+	if false:
+		pass
+	#if $GridMovement/RayCast2D.get_collider().is_in_group("player"):
+		#print("trigger scene")
+		#get_tree().paused = true
+	else:
+		if is_following:
+		# follow player if close enough
+			$Timer.paused = true
+			direction = global_position.direction_to(%Player.global_position)
+		else:
+		# randomized walking
+			$Timer.paused = false
+		$GridMovement.move(direction)
+		moving_animation(direction)
 			
 func moving_animation(input_direction: Vector2) -> void:
-	var animation_state: StringName = animation
+	var animation_state: StringName = $AnimatedSprite2D.animation
 	var moving_direction: Vector2 = $GridMovement.moving_direction
 	var vectorDirection = vector2Direction(moving_direction)
 	
@@ -31,18 +47,34 @@ func moving_animation(input_direction: Vector2) -> void:
 			vectorDirection = animation_state.get_slice("_", 1)
 			animation_state = "idle_" + vectorDirection
 			
-	play(animation_state)
+	$AnimatedSprite2D.play(animation_state)
  
 func vector2Direction(vec: Vector2) -> String:
 	var direction = "down"
 	if vec.y > 0: direction = "down"
 	elif vec.y < 0: direction = "up"
 	elif vec.x > 0:
-		flip_h = true
+		$AnimatedSprite2D.flip_h = true
 		direction = "side"
 	elif vec.x < 0:
 		# Horizontal flip since we have one animation for both left and right walking and idle
-		flip_h = false
+		$AnimatedSprite2D.flip_h = false
 		direction = "side"
 		
 	return direction
+
+
+func _on_area_2d_body_entered(body):
+	if body.name == "Player":
+		is_following = true
+
+
+func _on_area_2d_body_exited(body):
+	if body.name == "Player":
+		is_following = false
+
+
+func _on_timer_timeout():
+	# change direction of random enemy walking pattern
+	var random_index = randi() % movements.size()
+	direction = movements[random_index]
