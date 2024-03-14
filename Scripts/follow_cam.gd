6981a70af537d788a3056f5be5f2a7686299745c
chengrace <g.chen@rutgers.edu>
2024-03-14 14:53:58 -0400

    got rid of print statements in camera script

--- Scripts/follow_cam.gd
+++ Scripts/follow_cam.gd
@@ -8,7 +8,3 @@ func _ready():
 	var worldSizeInPixels = mapRect.size * tileSize
 	limit_right = worldSizeInPixels.x
 	limit_bottom = worldSizeInPixels.y
-	print(limit_top)
-	print(limit_bottom)
-	print(limit_right)
-	print(limit_left)
