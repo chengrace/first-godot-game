ec35980fc0d1f968901ecab477777eb46e1a7aab
chengrace <g.chen@rutgers.edu>
2024-03-20 13:29:46 -0400

    main menu complete without saving/loading functionality

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -2,14 +2,16 @@
 
 extends Node2D
 
-@onready var animation_player = $AnimationPlayer
+@onready var animation_player = $Transition
 
 func _on_new_game_pressed():
-	get_tree().change_scene_to_file("res://Scenes/house_area.tscn")
 	animation_player.play("fade_out")
 
 func _on_load_game_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/house_area.tscn")
 
 func _on_quit_game_pressed():
 	get_tree().quit()
+
+func _on_transition_animation_finished(anim_name):
+	get_tree().change_scene_to_file("res://Scenes/house_area.tscn")
