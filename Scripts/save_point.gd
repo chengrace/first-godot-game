16f5c210937f925240e7c88219b1b41f6caa2ea5
chengrace <g.chen@rutgers.edu>
2024-03-24 21:37:10 -0400

    fixed collisions stuff
    realized player can get close on right hand side but is far compared to left and top..need to fix that somewhere

--- Scripts/save_point.gd
+++ Scripts/save_point.gd
@@ -1,6 +1,11 @@
 # save_point.gd
 extends RigidBody2D
 
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _ready():
+	animated_sprite.play("default")
+
 #func _input(event):
 	#if event.is_action_pressed("ui_attack"):
 		#print("trigger save scene")
