23c89ed82ab20aba802c407594f4af60a7985cb7
chengrace <g.chen@rutgers.edu>
2024-04-15 15:52:55 -0400

    added main menu animation

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -9,9 +9,21 @@ var skip_first_button_sound = false #true when completed
 var stop_input = false
 
 func _ready():
+	$AnimationPlayer.play("slide_in")
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	skip_first_button_sound = false
 	new_game_button.grab_focus()
+
+func process():
+	# While animation is playing, don't accept any button input
+	if !$Timer.is_stopped():
+		set_process_input(false)
+
+func _input(event):
+	if !$Timer.is_stopped():
+		if event.is_action_pressed("ui_accept") or event.is_action_pressed("ui_focus_next") or event.is_action_pressed("ui_focus_prev"):
+			$AnimationPlayer.set_speed_scale(7)
+			button_sound.play()
 
 func _on_new_game_pressed():
 	Global.change_scene_with_transition("res://Scenes/house_area.tscn", "dissolve")
